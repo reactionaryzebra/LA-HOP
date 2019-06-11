5ee2a8a9402cc32289f41ded5cfc77476066d7ea
reactionaryzebra <theSource@Justins-MacBook-Air.local>
2019-06-11 12:59:13 -0700

    Add Routes for basic pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import * as ROUTES from "./constants/routes";
 
 function App() {
   return (
-    <div className="App">
-      <div>Hello World</div>
-    </div>
+    <Router>
+      <Route exact path={ROUTES.LANDING} render={() => <div>Landing</div>} />
+      <Route exact path={ROUTES.REPORT} render={() => <div>Report</div>} />
+      <Route
+        exact
+        path={ROUTES.CONFIRMATION}
+        render={() => <div>Confirmation</div>}
+      />
+      <Route
+        exact
+        path={ROUTES.REPORT_TRACKER}
+        render={() => <div>Route Tracker</div>}
+      />
+    </Router>
   );
 }
 
