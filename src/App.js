5e21ee0fa2529ff7506653bd55a2e9cbf9f8de8c
reactionaryzebra <theSource@Justins-MacBook-Air.local>
2019-06-11 20:14:23 -0700

    Add skeleton for LandingPage Component

--- src/App.js
+++ src/App.js
@@ -4,15 +4,16 @@ import Map from "./components/Map";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { withFirebase } from "./components/Firebase";
 import * as ROUTES from "./constants/routes";
-import Camera from './components/Camera'
-import Form from './components/Form'
+import Camera from "./components/Camera";
+import Form from "./components/Form";
 
 import FollowupForm from "./components/FollowupForm";
+import LandingPage from "./components/LandingPage";
 
 function AppBase() {
   return (
     <Router>
-      <Route exact path={ROUTES.LANDING} render={() => <div>Landing</div>} />
+      <Route exact path={ROUTES.LANDING} component={LandingPage} />
       <Route exact path={ROUTES.REPORT} component={Map} />
       <Route exact path={ROUTES.CONFIRMATION} component={FollowupForm} />
       <Route
@@ -20,16 +21,8 @@ function AppBase() {
         path={ROUTES.REPORT_TRACKER}
         render={() => <div>Route Tracker</div>}
       />
-      <Route
-        exact
-        path={ROUTES.CAMERA}
-        render={() => <Camera/>}
-      />
-      <Route
-        exact
-        path={ROUTES.FORM}
-        render={() => <Form/>}
-      />
+      <Route exact path={ROUTES.CAMERA} render={() => <Camera />} />
+      <Route exact path={ROUTES.FORM} render={() => <Form />} />
     </Router>
   );
 }
---
+++ src/components/LandingPage/index.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { withRouter } from "react-router-dom";
+
+const LandingPageBase = props => (
+  <div>
+    <header>
+      <p>
+        LA-HOP is designed to assist people experiencing homelessness in Los
+        Angeles County with outreach services. We'll use this information to
+        dispatch a homeless services outreach team to the area.
+      </p>
+    </header>
+    <div>
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+    </div>
+    <div>
+      <div>
+        <p>For medical or mental health emergencies, please call 911.</p>
+      </div>
+      <div>
+        <p>
+          For Services like bulky item pickup, illegal dumping or graffiti,
+          please contact your municipality
+        </p>
+      </div>
+      <div>
+        <p>
+          For crime or illegal activity, please contact your local law
+          enforcement agency
+        </p>
+      </div>
+      <div>
+        <p>
+          LA-HOP does not replace homeless encampment reporting protocols.
+          Please contact your municipality
+        </p>
+      </div>
+    </div>
+    <div>
+      <button
+        onClick={() => {
+          props.history.push("/report");
+        }}
+      >
+        MAKE A REQUEST
+      </button>
+      <button>LEARN MORE</button>
+    </div>
+  </div>
+);
+
+const LandingPage = withRouter(LandingPageBase);
+
+export default LandingPage;
